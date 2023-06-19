09177c2f8f774bcb7082e3fa40ff84c629f40452
Sbnam Ahmed Siyam <sabnamahmedsiyam263#gmail.com>
2023-06-19 22:02:35 +0600

    Loader & more data added

--- js/index.js
+++ js/index.js
@@ -1,30 +1,31 @@
-console;
-const url = `https://openapi.programming-hero.com/api/ai/tools`;
-fetch(url)
-  .then((res) => res.json())
-  .then((data) => {
-    showAiData(data.data.tools);
-  });
+// LoadData Function:
+const LoadData = () => {
+  loader(true);
+  fetch(`https://openapi.programming-hero.com/api/ai/tools`)
+    .then((res) => res.json())
+    .then((data) => {
+      const allData = data;
+      showAiData(allData.data.tools.slice(0, 6));
+    });
+};
 
-//show the data to UI
+// Showing data to the UI:
 const showAiData = (data) => {
-  // console.log(data)
-  const sliceData = data.slice(0,6).forEach((Ai) => {
-    // console.log(Ai)
-    const { name,image, published_in, features} = Ai;
+  loader(true);
+  data.forEach((Ai) => {
+    const { name, image, published_in, features, id } = Ai;
     const div = document.createElement("div");
-    const container = document.getElementById("container");
     div.innerHTML = `
-        <div class="card shadow-xl bg-slate-100 rounded-2xl mt-5 h-full  ">
+        <div class="card shadow-xl bg-slate-100 rounded-2xl h-full  ">
         <figure class="px-10 max-h-80 pt-10">
-            <img src="${image}" alt="Shoes" class="rounded-2xl" />
+            <img src="${image}" class="rounded-2xl" />
           </figure>
           <div class="card-body ">
             <div>
             <h2 class="card-title font-bold">Features</h2>
             <span class"text-[#585858]"><p>1.${features[0]}</p>
             <p>2.${features[1]}</p>
-            <p>3.${features[2] ? features[2] :'Text generation' }</p>
+            <p>3.${features[2] ? features[2] : "Text generation"}</p>
             </span>
             </div>
             <hr>
@@ -38,4 +39,53 @@ const showAiData = (data) => {
         `;
     container.appendChild(div);
   });
+  loader(false);
+  // setTimeout(loader, 500);
 };
+
+// Loader Function:
+const loader = (isLoading) => {
+  const LoaderSection = document.getElementById("loader");
+  if (isLoading) {
+    LoaderSection.classList.remove("hidden");
+  } else {
+    LoaderSection.classList.add("hidden");
+  }
+};
+
+
+// Showing More Data Function:
+const seeMoredata = () => {
+  emtyInnerHTML();
+  loader(true);
+  const SeeMoreBtn = document.getElementById("btn-seeMore");
+  SeeMoreBtn.classList.add("hidden");
+  fetch(`https://openapi.programming-hero.com/api/ai/tools`)
+    .then((res) => res.json())
+    .then((data) => {
+      showAiData(data.data.tools);
+      document.getElementById("btn-seeLess").classList.remove("hidden");
+      document.getElementById("btn-seeMore").classList.add("hidden");
+    });
+};
+
+// seeLessData Function:
+const seeLessData = () => {
+  emtyInnerHTML();
+  loader(true);
+  loader(true);
+  loader(true);
+  loader(true);
+  LoadData();
+  document.getElementById("btn-seeLess").classList.add("hidden");
+  document.getElementById("btn-seeMore").classList.remove("hidden");
+};
+
+// emtyInnerHTML Function:
+const emtyInnerHTML = () => {
+  const container = document.getElementById("container");
+  container.innerHTML = "";
+};
+
+// Calling the LoadData function:
+LoadData();
